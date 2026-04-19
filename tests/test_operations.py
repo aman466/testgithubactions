d2708726cd7eaaa8117ff87f0a9f01fdc241ceb2
Aman Kumar <aman.kumar466@gmail.com>
2026-04-20 02:10:12 +0530

    Unit tests update

---
+++ tests/test_operations.py
@@ -0,0 +1,10 @@
+from src.math_operations import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+    assert subtract(1, 2) == -1
+    assert subtract(-1, 1) == -2
